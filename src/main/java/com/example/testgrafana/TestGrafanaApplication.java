a6ebbf5d097cb734ddb124e266f17afa84838da7
marzieh1372 <m.abbasifarvad@gmail.com>
2023-04-03 15:59:11 +0330

    Initial Commit

---
+++ src/main/java/com/example/testgrafana/TestGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.testgrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGrafanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGrafanaApplication.class, args);
+    }
+
+}
